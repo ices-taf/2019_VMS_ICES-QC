9e0ac94f41251298badbaddc030fc8c4a19a4693
colin millar <colinpmillar@gmail.com>
2019-05-02 10:39:50 +0200

    NOR not NO

--- bootstrap.R
+++ bootstrap.R
@@ -4,23 +4,20 @@
 ## After:
 
 library(icesTAF)
+library(RODBC)
+library(jsonlite)
+
 
 mkdir("bootstrap/data")
 
 
 # packages
-library(RODBC)
-library(icesTAF)
-library(jsonlite)
 
 # utilities
 source("utilities.R")
 
 # settings
 config <- read_json("bootstrap/config/config.json", simplifyVector = TRUE)
-
-# create directories
-mkdir(config$data_dir)
 
 # connect to DB
 conn <- odbcDriverConnect(connection = config$db_connection)
@@ -38,7 +35,7 @@ for (country in config$countries) {
   msg("downloading LE data for ... ", country)
 
   # set up sql command
-  sqlq <- sprintf("SELECT * FROM dbo._ICES_VMS_Datacall_LE WHERE country = '%s' order by year, ICES_rectangle, gear_code, LE_MET_level6, month, vessel_length_category, fishing_days, vms_enabled", country)
+  sqlq <- sprintf("SELECT * FROM [dbo].[_ICES_VMS_Datacall_LE] WHERE country = '%s'", country)
   fname <- paste0("bootstrap/data/ICES_LE_", country, ".csv")
 
   # fetch
@@ -52,11 +49,7 @@ for (country in config$countries) {
   msg("downloading VMS data for ... ", country)
 
   # set up sql command
-  if (country == "NO") {
-    sqlq <- "SELECT * FROM [dbo].[_ICES_VMS_Datacall_VMS] WHERE country in ('NOR', 'NO') order by year, c_square, gear_code, LE_MET_level6, month, vessel_length_category, fishing_hours, avg_fishing_speed"
-  } else {
-    sqlq <- sprintf("SELECT * FROM [dbo].[_ICES_VMS_Datacall_VMS] WHERE country = '%s'order by year, c_square, gear_code, LE_MET_level6, month, vessel_length_category, fishing_hours, avg_fishing_speed", country)
-  }
+  sqlq <- sprintf("SELECT * FROM [dbo].[_ICES_VMS_Datacall_VMS] WHERE country = '%s'", country)
   fname <- paste0("bootstrap/data/ICES_VE_", country, ".csv")
 
   # fetch
